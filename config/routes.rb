f2e0ab579745334ed25f8c7ce3a6a14a72eeae3f
Maxi-chelsea <maxi19950049@gmail.com>
2021-05-18 12:08:40 +0000

    deploy-setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :employees
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
 
   resources :employees, only: [:index, :show, :new, :create, :edit, :update]
-  scope :facility do
-    resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  end
-
   resources :facilities, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :dairy_reports
 end
